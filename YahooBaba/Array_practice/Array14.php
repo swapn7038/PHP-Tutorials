e99960ee4719383f633d3c62a2a77a5af5b29e4d
Swapnil Shelke <Swapnilbabanshelke@gmail.com>
2023-01-02 18:56:09 +0530

    Array_slice

---
+++ YahooBaba/Array_practice/Array14.php
@@ -0,0 +1,15 @@
+<?php
+// Video 44 : Yahoo Baba
+
+// array_slice() 
+
+$a = ["a", "b", "c", "d"];
+
+// syntax 
+// array_slice(array, start, length);
+
+$newArray = array_slice($a, -2, 1);
+
+print_r($newArray);
+
+?>
